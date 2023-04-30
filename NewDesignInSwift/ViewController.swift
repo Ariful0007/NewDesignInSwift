97391061e5acfaa82854b3eabd69c2d543be1e0c
Ariful0007 <arifulpub143@gmail.com>
2023-04-30 16:11:32 +0600

    main

--- NewDesignInSwift/ViewController.swift
+++ NewDesignInSwift/ViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 import SwiftUI 
+class UILabel : UIView
+{
 
+}
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var selectImage: UIImageView!
+    @IBOutlet weak var enterPassword: UITextField!
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var enterEmail: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
